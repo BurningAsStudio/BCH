c2618e3b044cdfb7acf3097b2b8b784cd8df5f32
Andrii <166237939+BurningAsStudio@users.noreply.github.com>
2025-01-28 18:14:20 +0200

    created basic phone input

--- Assets/Added Assets/AtmosphericHouse/Scripts/SimplePlayerUse.cs
+++ Assets/Added Assets/AtmosphericHouse/Scripts/SimplePlayerUse.cs
@@ -1,44 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 namespace FS_Atmo
 {
 
-    public class SimplePlayerUse : MonoBehaviour
-    {
-        public GameObject mainCamera;
-        private GameObject objectClicked;
-        public GameObject flashlight;
-        public KeyCode OpenClose;
-        public KeyCode Flashlight;
+	public class SimplePlayerUse : MonoBehaviour
+	{
+		[SerializeField] private Button _flashlightButton;
+		[SerializeField] private Button _interactButton;
+		[SerializeField] private GameObject _mainCamera;
+		[SerializeField] private GameObject _flashlight;
+		[SerializeField] private float _rayDistance = 2.3f;
 
-        void Start()
+		private void OnEnable()
+		{
+			_flashlightButton.onClick.AddListener(SwitchFlashlight);
+			_interactButton.onClick.AddListener(Interact);
+		}
+		
+		private void OnDisable()
+		{
+			_flashlightButton.onClick.AddListener(SwitchFlashlight);
+			_interactButton.onClick.AddListener(Interact);
+		}
+
+		private void Interact()
+		{
+			RaycastCheck();
+		}
+
+		private void SwitchFlashlight()
+		{
+			if (_flashlight.activeSelf)
+				_flashlight.SetActive(false);
+			else
+				_flashlight.SetActive(true);
+		}
+
+		private void RaycastCheck()
         {
-
-        }
-
-        void Update()
-        {
-            if (Input.GetKeyDown(OpenClose)) // Open and close action
-            {
-                RaycastCheck();
-            }
-
-            if (Input.GetKeyDown(Flashlight)) // Toggle flashlight
-            {
-                if (flashlight.activeSelf)
-                    flashlight.SetActive(false);
-                else
-                    flashlight.SetActive(true);
-            }
-        }
-
-        void RaycastCheck()
-        {
-            RaycastHit hit;
-
-            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out hit, 2.3f))
+	        if (Physics.Raycast(_mainCamera.transform.position,
+		            _mainCamera.transform.TransformDirection(Vector3.forward), out var hit, _rayDistance))
             {
                 if (hit.collider.gameObject.GetComponent<SimpleOpenClose>())
                 {
